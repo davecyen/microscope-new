57d2c886ca7e689f12355f7a7e3685bc3558b292
Dave <davecyen@gmail.com>
2013-12-08 01:28:47 -0800

    Fixed post submit bug

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -4,7 +4,7 @@ Template.postSubmit.events({
 
     var post = {
       url: $(e.target).find('[name=url]').val(),
-      title: $(e.target).find('name=title]').val(),
+      title: $(e.target).find('[name=title]').val(),
       message: $(e.target).find('[name=message]').val()
     };
 
